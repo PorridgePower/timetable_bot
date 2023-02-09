967e770c4dab39bbe9eb236964d7d3c9228c4c90
PorridgePower <rock-with-me@yandex.ru>
2023-02-09 04:28:50 -0500

    Conversation handling

--- timetablebot.py
+++ timetablebot.py
@@ -9,8 +9,14 @@ from telegram.ext import (
     MessageHandler,
     filters,
     CallbackQueryHandler,
+    ConversationHandler,
 )
 import sheets
+
+# states
+START, SPOT, TIMETABLE = range(3)
+# callback returning values
+SUB, BACK = range(2)
 
 # Enable logging
 logging.basicConfig(
@@ -29,21 +35,45 @@ async def start(timetable, update: Update, context: ContextTypes.DEFAULT_TYPE) -
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text(
-        f"Hi {user.full_name}! Select a subsidiary:", reply_markup=reply_markup
-    )
+    if update.message is not None:
+        await update.message.reply_text(
+            f"Hi {user.full_name}! Select a subsidiary:", reply_markup=reply_markup
+        )
+    else:
+        query = update.callback_query
+        await query.answer()
+        await update.callback_query.message.edit_text(
+            f"Hi {user.full_name}! Select a subsidiary:", reply_markup=reply_markup
+        )
+    return SPOT
 
 
-async def callback_button(timetable, update, context):
+async def choose_place_btn_callback(timetable, update, context):
     query = update.callback_query
+    await query.answer()
     table = "\n".join(
         [
             "%s:   %s" % (day, "   ".join(hours))
             for day, hours in timetable[query.data].items()
         ]
     )
+    keyboard = [
+        [
+            InlineKeyboardButton("Subsctibe to this subsidiary", callback_data=SUB),
+            InlineKeyboardButton("Back", callback_data=BACK),
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(f"{table}", reply_markup=reply_markup)
+    return TIMETABLE
+
+
+async def subscribe_btn_callback(timetable, update, context):
+    # this is stub
+    query = update.callback_query
     await query.answer()
-    await query.edit_message_text(text=f"{table}")
+    return TIMETABLE
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -62,13 +92,27 @@ def main() -> None:
     """Start the bot."""
     application = Application.builder().token(Config.TELEGRAM_TOKEN).build()
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", partial(start, timetable)))
-    application.add_handler(CallbackQueryHandler(partial(callback_button, timetable)))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", partial(start, timetable))],
+        states={
+            # SPOT - Shows places
+            SPOT: [
+                CallbackQueryHandler(partial(choose_place_btn_callback, timetable)),
+            ],
+            # TIMETABLE - Shows timetable for selected place
+            TIMETABLE: [
+                CallbackQueryHandler(
+                    partial(subscribe_btn_callback, timetable),
+                    pattern="^" + str(SUB) + "$",
+                ),
+                CallbackQueryHandler(
+                    partial(start, timetable), pattern="^" + str(BACK) + "$"
+                ),
+            ],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+    application.add_handler(conv_handler)
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
